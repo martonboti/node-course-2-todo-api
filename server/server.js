7ec9ac24a0608697181f3ae79f19057182d3cb72
unknown <martonbotix@gmail.com>
2018-04-26 10:30:16 +0300

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,11 @@ app.post('/todos', (req, res) => {
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => res.status(422).send(e));
+});
 
 
 app.listen(3000, () => {
